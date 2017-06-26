5eff5c9e14690df726080fa12b3db968b1a59ecc
pietrodev93 <pietrogiordano93@gmail.com>
2017-06-26 19:34:15 +0200

    a

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
   extend FriendlyId
-  friendly_id :titolo, use: :slugged :history
+  friendly_id :titolo, use: :slugged
   has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
   attr_accessor :slug
