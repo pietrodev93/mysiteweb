5edf73234264929d2ef642d504a1eb57aac7ee37
pietrodev93 <pietrogiordano93@gmail.com>
2017-06-26 19:43:24 +0200

    region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    bucket: ENV.fetch('pietrogiordano'),
+    access_key_id: ENV.fetch('AKIAI5KOE5XUQYMVTXFQ'),
+    secret_access_key: ENV.fetch('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'),
+    s3_region: ENV.fetch('EU'),
   }
 }
 
